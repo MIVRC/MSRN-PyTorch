a0985b509b2260369bc23b0d0e5f8fe7530eddbe
MIVRC <ecnuljc@gmail.com>
2018-03-21 11:39:28 +0800

    new files

--- data.py
+++ data.py
@@ -11,13 +11,11 @@ class DatasetFromHdf5(data.Dataset):
         super(DatasetFromHdf5, self).__init__()
         dataset = h5py.File(file_path)
         # input|hr_x2|hr_x4|hr_x8|bicubic_x2|bicubic_x4|bicubic_x8
-        # 分别代表输入的LR、2倍放大的HR、4倍放大的HR、8倍放大的HR、2倍bicubic放大的SR、4倍bicubic放大的SR、8倍bicubic放大的SR
         # input:32*32; 2x:64*64; 4x:128*128; 8x:256*256
         self.data = dataset.get("data")
         self.label = dataset.get("label")
 
     def __getitem__(self, index):
-        # 将数从numpy转为torch
         return torch.from_numpy(self.data[index, :, :, :]).float(), \
                torch.from_numpy(self.label[index, :, :, :]).float()
 
